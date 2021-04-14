b902e9f06e67dc3fb61aee345d01013986212a4b
Emil Colina <cybre333@gmail.com>
2021-04-14 12:04:02 -0400

    inicio de proyecto, configuración de puerto analogo

---
+++ analisis_HFC.c
@@ -0,0 +1,16 @@
+#include <16f887.h>
+#device adc = 10
+#use DELAY(clock = 4000000)
+#byte porta = 0x05
+#fuses xt, NOPROTECT, NOWDT, NOBROWNOUT, PUT, NOLVP
+
+#byte porta = 0x05
+
+// variables generales
+int delay = 200;
+
+int main()
+{
+    setup_adc(adc_clock_internal); //configuración de puerto analogo
+    setup_adc(all_analog);
+}
